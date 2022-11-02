aa5fd32179cfe652b690c5fd3df07108cd8a275e
Teddy Minaya <teddyminaya@gmail.com>
2022-11-02 14:01:35 -0400

    commit tree c69ca6d6d9e725c7669811e6a4b3908be2320388

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) { 
+    console.log('HELLO');
+}
+
+function logWhisper(string) { 
+    console.log('hello');
+}
+
+function sayHiToHeadphonedRoommate(string){
+if (string===toLowerCase)
+console.log ("i can't hear you!")
+else (string===toUpperCase)
+console.log ("YES INDEED")}
+
+function sayHiToHeadphonedRoommate(string) {
+        if (string === string.toLowerCase()) {
+          return "I can't hear you!";
+      }
+      if (string === string.toUpperCase()) {
+          return "YES INDEED!";
+      } if ("let's have dinner together")
+        return ("I would love to!")
+}
+    
